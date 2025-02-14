8dac73b5bb49f9d13d735e78536775b36aaeff63
DP투데커 <jm40108@gmail.com>
2025-02-14 11:46:20 +0900

    haha

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -13,6 +13,16 @@ public class test : Status
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Input.mousePosition);
+        if (Input.mousePosition.x < Screen.width * 0.1f)
+        {
+            Camera.main.transform.Translate(Vector3.left * Time.deltaTime * 3f);
+        }
+        if (Input.mousePosition.x > Screen.width * 0.9f)
+        {
+            Camera.main.transform.Translate(Vector3.right * Time.deltaTime * 3f);
+        }
+        Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, -7.8f, 7.8f), Camera.main.transform.position.y,
+            Camera.main.transform.position.z);
     }
 }
