883ad8b9c10bfd146780e4fc12664509d352307d
DP투데커 <jm40108@gmail.com>
2025-02-14 11:28:47 +0900

    YEah

--- Assets/Scripts/Status.cs
+++ Assets/Scripts/Status.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
-using static Status;
 
+
+[System.Serializable]
+public struct battleData
+{
+    public int attackPower;
+    public int hp;
+    public float attackSpeed;
+    public int cost;
+    public float moveSpeed;
+}
 public class Status : MonoBehaviour
 {
-    [System.Serializable]
-    public struct battleData
-    {
-        public int attackPower;
-        public int hp;
-        public float attackSpeed;
-        public int cost;
-        public float moveSpeed;
-    }
 
 }
